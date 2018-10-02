a15f3a2d47d577d89e0947373c56691c6169fbd7
Nicole D <nnardella@gmail.com>
2018-10-02 14:38:24 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,6 @@ def turn_count(board)
 counter = 0
 
 board.each do |space|
-
   if space == "X" || space == "O"
   counter += 1
 
@@ -19,7 +18,6 @@ def current_player(board)
 
 if turn_count(board) % 2 == 0
   current_player = "X"
-
 else
   current_player = "O"
 
